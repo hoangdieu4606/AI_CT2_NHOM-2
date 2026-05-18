4c58c915f99289a5f301824f05f4d7df3c36f963
nguyendoan31241026724-bit <nguyendoan.31241026724@st.ueh.edu.vn>
2026-05-18 16:07:20 +0700

    Update fitness.py

--- AIPROJECT VERSION 3/AIproject/core/fitness.py
+++ AIPROJECT VERSION 3/AIproject/core/fitness.py
@@ -12,11 +12,7 @@ def compute_value(solution: Iterable[int], instance: KnapsackInstance) -> float:
 
 
 def evaluate_solution(solution: List[int], instance: KnapsackInstance) -> Tuple[float, float, bool]:
-    """
-    Policy theo yêu cầu thầy:
-    - Nếu vượt capacity thì fitness/value = 0
-    - Không repair trong quá trình đánh giá
-    """
+    
     total_weight = compute_weight(solution, instance)
     total_value = compute_value(solution, instance)
     feasible = total_weight <= instance.capacity + 1e-9
